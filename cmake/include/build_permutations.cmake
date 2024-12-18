4d910accd0ed4b996bbf230554dfcf957d4db3d6
Philippe Coval <philippe.coval@silabs.com>
2024-12-18 09:42:41 +0100

    SWPROT-8953: Build zpc with uic as source dep on latest (not testing)
    Version 1.6.0 it not supported yet,
    but it can help to initiate the migration to next UnifySDK-1.7.
    
    Note that ctest will be enabled in later PR.
    
    Last-Update: 2025-01-21
    Origin: https://github.com/SiliconLabsSoftware/z-wave-protocol-controller/pull/13
    Signed-off-by: Philippe Coval <philippe.coval@silabs.com>

--- cmake/include/build_permutations.cmake
+++ cmake/include/build_permutations.cmake
@@ -1,21 +1,17 @@
 # Build options enabling different permutations and exluding unwanted software
+
+option(BUILD_TESTING "WARNING: Bypass all tests (for integration purpose only)" OFF)
+
 # applications
-option(FETCH_GSDK "Force CMAKE to fetch Gecko SDK from GitHub" OFF)
-option(BUILD_ZIGPC "Package the ZIGPC" OFF)
 option(BUILD_ZPC "Package the ZPC" ON)
 option(BUILD_DEV_GUI "Package the developer GUI" OFF)
 option(BUILD_UIC_DEMO "Package the Unify demo" OFF)
 option(BUILD_UPVL "Package the UIC-UPVL" OFF)
 option(BUILD_GMS "Package the UIC-GMS" OFF)
 option(BUILD_IMAGE_PROVIDER "Build the UIC-IMAGE-PROVIDER" OFF)
-option(BUILD_AOXPC "Package the AOXPC" OFF)
-option(BUILD_POSITIONING "Package the Positioning Service" OFF)
 option(BUILD_NAL "Package the Name and location service" OFF)
 option(BUILD_UPTI_CAP "Build the UIC-UPTI-CAP" OFF)
 option(BUILD_UPTI_WRITER "Build the UIC-UPTI-WRITER" OFF)
-option(BUILD_CPCD "Package CPCd" OFF)
-option(BUILD_ZIGBEED "Build Zigbeed" OFF)
-option(BUILD_OTBR "Build OpenThread Border Router" OFF)
 option(BUILD_MATTER_BRIDGE "Build the Matter Bridge" OFF)
 option(BUILD_EPC "Build EPC (Example Protocol Controller)" OFF)
-option(BUILD_EMD "Build EMD (Example MQTT Device)" OFF)
+option(BUILD_EED "Build EED (Emulated End Device)" OFF)

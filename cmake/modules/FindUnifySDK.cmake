7a07239395fdd84ee176453fe76157aa1b8ad424
Philippe Coval <philippe.coval@silabs.com>
2025-02-10 15:45:09 +0100

    SWPROT-8953: Disable Werror on UnifySDK dep release
    As explain in patch it is good to use Werror in devel or ci branch
    not in releases (tags).
    
    This change is not yet applied upstream,
    to support more compilers, issue has been reported on OSX using
    
    ```
    ➜  ~ cc --version
    Apple clang version 15.0.0 (clang-1500.3.9.4)
    Target: x86_64-apple-darwin23.4.0
    Thread model: posix
    InstalledDir: /Library/Developer/CommandLineTools/usr/bin
    ➜  ~ c++ --version
    Apple clang version 15.0.0 (clang-1500.3.9.4)
    Target: x86_64-apple-darwin23.4.0
    Thread model: posix
    InstalledDir: /Library/Developer/CommandLineTools/usr/bin
    ```
    
    I would be nice to fix this in the upstream too:
    
        .../unifysdk-src/components/uic_main/src/uic_init.c:77:21: \
        error: passing arguments to 'sl_log_read_config' \
        without a prototype is deprecated in all versions of C \
        and is not supported in C2x \
        [-Werror,-Wdeprecated-non-prototype]
        sl_log_read_config(NULL);
    
    Origin: https://github.com/SiliconLabsSoftware/z-wave-protocol-controller/pull/25
    Signed-off-by: Philippe Coval <philippe.coval@silabs.com>

--- cmake/modules/FindUnifySDK.cmake
+++ cmake/modules/FindUnifySDK.cmake
@@ -27,12 +27,25 @@ if("${UNIFYSDK_GIT_TAG}" STREQUAL "")
   set(UNIFYSDK_GIT_TAG "main") # Override CMake default ("master")
 endif()
 
+if(${GIT_EXECUTABLE})
+else()
+  set(GIT_EXECUTABLE git)
+endif()
+
 FetchContent_Declare(
   UnifySDK
   GIT_REPOSITORY ${UNIFYSDK_GIT_REPOSITORY}
   GIT_TAG        ${UNIFYSDK_GIT_TAG}
   GIT_SUBMODULES_RECURSE True
   GIT_SHALLOW 1
+
+  # Prevent "fatal: unable to auto-detect email address"
+  GIT_CONFIG user.email=nobody@UnifySDK.localhost
+
+  PATCH_COMMAND ${GIT_EXECUTABLE}
+      -C <SOURCE_DIR>
+      am
+      ${PROJECT_SOURCE_DIR}/patches/UnifySDK/0001-UIC-3202-Relax-compiler-warnings-to-support-more-com.patch
 )
 
 message(STATUS "${CMAKE_PROJECT_NAME}: Depends: ${UNIFYSDK_GIT_REPOSITORY}#${UNIFYSDK_GIT_TAG}")
